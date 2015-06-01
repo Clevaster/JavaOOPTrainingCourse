c7a174280f3301cb3fc99fffcbcc9481c031b54b
Yana Vayzer <cc2332005@gmail.com>
2015-06-01 12:56:55 +0300

    comments added

--- src/main/java/com/tyshchenko/training/java/oop/lesson6/ThreadGroupExample.java
+++ src/main/java/com/tyshchenko/training/java/oop/lesson6/ThreadGroupExample.java
@@ -9,62 +9,53 @@ import java.util.List;
 public class ThreadGroupExample {
 
     public static void main(String[] args) {
+
+
+  /*      //создаем лист дл€ хранени€ потоков.
         int threadsCount = 10;
         List<CounterThread> threads = new ArrayList<>(threadsCount);
 
+        //добавл€ем в него 10 (названиеЌомер) потоков и запускаем их
         for (int i = 0; i < threadsCount; i++) {
+                                                     //такое название будет у потока
             CounterThread thread = new CounterThread("MyThreadCounter" + i);
             threads.add(thread);
             thread.start();
-        }
+        }*/
+//-----------
 
-        // Java 8
-//        IntStream.range(1, 11).forEach(
-//                (a) ->  {
-//                    CounterThread thread = new CounterThread("MyThreadCounter" + a);
-//                    threads.add(thread);
-//                    thread.start();
-//                }
-//        );
-//
-        // Java 8
-//        IntStream.range(1, threadsCount).forEach((i) -> threads.add(CounterThread.getInstance("MyThreadCounter" + i)));
-//
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        for (CounterThread thread : threads) {
-//            thread.interrupt();
-//        }
-//
-//        // Java 8
-//        threads.forEach(t -> t.interrupt());
+//-----------
 
+        //Їкземпл€р класса  ThreadGroup
         ThreadGroup threadGroup = new ThreadGroup("MyThreadGroup");
 
+
+        //создаем потоки с конструктором, котор≥й добавл€ет их в группу, запускаем
         for (int i = 0; i < 10; i++) {
             CounterThread counterThread = new CounterThread(threadGroup, "MyThreadCounter" + i);
             counterThread.start();
         }
 
         try {
+            //мейн засыпает через 5 сек
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+        //прерываем группу
         threadGroup.interrupt();
     }
 
-    private static final class CounterThread extends Thread {
+    private static final class
+            CounterThread extends Thread {
 
+        //конструктор(им€)
         public CounterThread(String name) {
             super(name);
         }
 
+        //конструктор, принимает название групп≥, в которую входит поток и им€
         public CounterThread(ThreadGroup threadGroup, String name) {
             super(threadGroup, name);
         }
@@ -75,6 +66,9 @@ public class ThreadGroupExample {
             return thread;
         }
 
+        //поток (или группа потоков) ждет пока его/ее не прервут
+        //печатает им€ : счетчик
+        //спит секунду
         @Override
         public void run() {
             int x = 0;
@@ -90,3 +84,27 @@ public class ThreadGroupExample {
         }
     }
 }
+// Java 8
+//        IntStream.range(1, 11).forEach(
+//                (a) ->  {
+//                    CounterThread thread = new CounterThread("MyThreadCounter" + a);
+//                    threads.add(thread);
+//                    thread.start();
+//                }
+//        );
+//
+// Java 8
+//        IntStream.range(1, threadsCount).forEach((i) -> threads.add(CounterThread.getInstance("MyThreadCounter" + i)));
+//
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        for (CounterThread thread : threads) {
+//            thread.interrupt();
+//        }
+//
+//        // Java 8
+//        threads.forEach(t -> t.interrupt());
